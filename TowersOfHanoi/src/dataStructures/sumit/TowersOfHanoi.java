3cca04face5c812b25842e14b831fcbb588bfc15
RanaSumit <rana91sumit@gmail.com>
2016-01-03 00:20:54 -0800

    Towers Of Hanoi
    Takes n Disks as input and moves n disks from Tower A to Tower C, Using
    Tower B as auxiliary.

---
+++ TowersOfHanoi/src/dataStructures/sumit/TowersOfHanoi.java
@@ -0,0 +1,29 @@
+package dataStructures.sumit;
+
+import java.util.Scanner;
+
+public class TowersOfHanoi {
+
+    public void logic (int n, String start, String aux, String dest)
+    {
+        if(n==1)
+        {
+            System.out.println(start + "->" + dest);
+        }
+        else
+        {
+            logic(n-1, start, dest, aux);
+            System.out.println(start + "->" + dest);
+            logic(n-1, aux, start, dest);
+        }
+
+    }
+
+    public static void main(String[] args) {
+	TowersOfHanoi TOH = new TowersOfHanoi();
+        System.out.println("Enter number if disks: ");
+        Scanner scanner = new Scanner(System.in);
+        int discs = scanner.nextInt();
+        TOH.logic(discs,"A", "B", "C");
+    }
+}
