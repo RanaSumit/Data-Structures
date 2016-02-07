eec11de4592f23798505828a5f7f800f5f9c7ff1
RanaSumit <rana91sumit@gmail.com>
2016-02-07 13:09:20 -0800

    Longest Common Subsequence

---
+++ LCS/Java/src/com/sumit/Main.java
@@ -0,0 +1,43 @@
+package com.sumit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader StdIn = new BufferedReader(new InputStreamReader(System.in));
+        String x = StdIn.readLine();
+        String y = StdIn.readLine();
+        int M = x.length();
+        int N = y.length();
+
+        // opt[i][j] = length of LCS of x[i..M] and y[j..N]
+        int[][] opt = new int[M+1][N+1];
+
+        // compute length of LCS and all subproblems via dynamic programming
+        for (int i = M-1; i >= 0; i--) {
+            for (int j = N-1; j >= 0; j--) {
+                if (x.charAt(i) == y.charAt(j))
+                    opt[i][j] = opt[i+1][j+1] + 1;
+                else
+                    opt[i][j] = Math.max(opt[i+1][j], opt[i][j+1]);
+            }
+        }
+
+        // recover LCS itself and print it to standard output
+        int i = 0, j = 0;
+        while(i < M && j < N) {
+            if (x.charAt(i) == y.charAt(j)) {
+                System.out.print(x.charAt(i));
+                i++;
+                j++;
+            }
+            else if (opt[i+1][j] >= opt[i][j+1]) i++;
+            else                                 j++;
+        }
+        System.out.println();
+
+    }
+}
