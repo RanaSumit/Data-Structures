9e112bf4636aaf6759bf0fa63212f63a5895eeb8
RanaSumit <rana91sumit@gmail.com>
2016-02-14 04:16:02 -0800

    Updated

--- Cloudera/PostOrder Evaluation/Java/src/com/sumit/Solution.java
+++ Cloudera/PostOrder Evaluation/Java/src/com/sumit/Solution.java
@@ -3,128 +3,45 @@ import java.util.*;
 
 public class Solution {
 
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
-		String str = new Scanner(System.in).nextLine();
-		System.out.print(sol(str));
+		String expression = new Scanner(System.in).nextLine();
+		expression = expression.replaceAll(" ","");
+		int result=evaluatePostfixExp(expression);
+		System.out.println(result);
 	}
 
-	public static int sol(String S) {
+	private static int evaluatePostfixExp(String expression) {
+		java.util.Stack<Integer> st =
+				new java.util.Stack<Integer>();
+		int i=0;
+		int len= expression.length();
 
-		// write your code in Java SE 8
-
-		char[] tokens = S.toCharArray();
-
-		// Stack for numbers: 'values'
-
-		Stack<Integer> values = new Stack<Integer>();
-
-
-
-		for (int i = 0; i < tokens.length; i++)
-
+		char[] ch = expression.toCharArray();
+		while(i<len)
 		{
 
-			// Current token is a whitespace, skip it
-
-			if (tokens[i] == ' ')
-
-				continue;
-
-
-
-			// Current token is a number, push it to stack for numbers
-
-			if (tokens[i] >= '0' && tokens[i] <= '9')
-
+			if(ch[i]=='+' || ch[i]=='-' || ch[i]=='*' || ch[i]=='/' )
 			{
-
-				values.push(Integer.parseInt(tokens[i]+""));
-
-			}
-
-
-
-			// Current token is an operator.
-
-			else if (tokens[i] == '+' || tokens[i] == '*' )
-
-			{
-
-				try{
-
-					int intermediateResult=applyOp(tokens[i], values.pop(), values.pop());
-
-
-
-					if(intermediateResult <= 4095)
-
-						values.push(intermediateResult);
-
-					else
-
-						return -1;
-
-
-
-				}catch(EmptyStackException e){
-
-					return -1 ;
+				int num2=st.pop();
+				int num1=st.pop();
+				switch(ch[i])
+				{
+					case '+':st.push(num1+num2);break;
+					case '-':st.push(num1-num2);break;
+					case '*':st.push(num1*num2);break;
+					case '/':st.push(num1/num2);break;
 
 				}
-
 			}
+			else
+			{
 
+				st.push(Character.getNumericValue(ch[i]));
+			}
+			i++;
 		}
-
-
-
-		if(values.isEmpty())
-
-			return -1;
-
-
-
-		return values.pop();
-
-	}
-
-
-
-	public static int applyOp(char op, int b, int a)
-
-	{
-
-		switch (op)
-
-		{
-
-			case '+':
-
-				return a + b;
-
-			case '-':
-
-				return a - b;
-
-			case '*':
-
-				return a * b;
-
-			case '/':
-
-				if (b == 0)
-
-					throw new
-
-							UnsupportedOperationException("Cannot divide by zero");
-
-				return a / b;
-
-		}
-
-		return 0;
-
+		return (Integer)st.pop();
 	}
 
 }
